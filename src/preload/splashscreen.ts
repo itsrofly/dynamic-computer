8b847dadc152091df4c973b7011df6e1f19893c8
itsrofly <rocketeman1234@gmail.com>
2025-01-22 22:47:18 +0000

    Splashscreen flashing text

--- src/preload/splashscreen.ts
+++ src/preload/splashscreen.ts
@@ -1,7 +1,9 @@
-import { ipcRenderer } from "electron";
+import { ipcRenderer } from 'electron'
 
 ipcRenderer.on('dependencies:progress', (_event, percentage, message) => {
-    const progressBar = document.getElementById('progress-bar') as HTMLDivElement
-    progressBar.style.width = `${percentage}%`
-    progressBar.innerText = message
+  const progressBar = document.getElementById('progress-bar') as HTMLDivElement
+  progressBar.style.width = `${percentage}%`
+
+  const progressText = document.getElementById('progress-bar-text') as HTMLSpanElement
+  progressText.innerText = message
 })
